40852f8cb7da162c0f50439b139997ec8bf9df8b
feicien <ithcheng@gmail.com>
2022-05-17 21:30:50 +0800

    update kotlin version  1.4.10 => 1.6.21

--- module-kotlin/src/main/java/com/alibaba/android/arouter/demo/kotlin/KotlinTestActivity.kt
+++ module-kotlin/src/main/java/com/alibaba/android/arouter/demo/kotlin/KotlinTestActivity.kt
@@ -2,10 +2,10 @@ package com.alibaba.android.arouter.demo.kotlin
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
-import kotlinx.android.synthetic.main.activity_kotlin_test.*
 
 @Route(path = "/kotlin/test")
 class KotlinTestActivity : Activity() {
@@ -21,6 +21,7 @@ class KotlinTestActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_test)
 
-        content.text = "name = $name, age = $age"
+        val mContentView: TextView = findViewById(R.id.content)
+        mContentView.text = "name = $name, age = $age"
     }
 }
